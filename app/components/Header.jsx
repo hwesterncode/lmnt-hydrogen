cace4ab194e6e52625aed3238ad2eae6a0452a98
hwesterncode <harrison@westerncoder.com>
2024-11-08 11:46:53 -0700

    Changed Site Title

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,7 +11,7 @@ export function Header({header, isLoggedIn, cart, publicStoreDomain}) {
   return (
     <header className="header">
       <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
-        <strong>{shop.name}</strong>
+        <strong>Western Rodeo Store</strong>
       </NavLink>
       <HeaderMenu
         menu={menu}
